8ab562fc78f60cc04220d85b5261b48b8cf2cbb3
Nathan Mugnier <nathan.mugnier@wanadoo.fr>
2018-10-03 15:27:35 +0200

    Ball movement

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,26 +4,13 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-    public int health = 0;
-    public Material[] healthMaterials = new Material[4];
-    public Renderer m_CurrentRenderer = null;
-
-    private void OnCollisionEnter(Collision iCollision)
-    {
-        if(iCollision.gameObject.CompareTag("Ball"))
-        {
-            health--;
-        }
-    }
-
-    private void TakeDamage(int iNbDamage = 1)
-    {
-        health -= iNbDamage;
-        if(health == 0)
-        {
-            Destroy(gameObject);
-        }
-
-        m_CurrentRenderer.material = healthMaterials[health - 1];
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
